1dece0b5907dc29dfa0d34596b3ead81fc20701d
Cosmin <cosmiin.mihalescull@gmail.com>
2020-04-19 13:49:22 +0300

    how old are you ? like this is called my first problem

---
+++ index.js
@@ -0,0 +1,11 @@
+//get a age and convert in to a Number;
+var age = Number(prompt("What is your age, buddy?"));
+
+//if age is too small
+if(age < 18) {
+    console.log("You're so younggg !!");
+}
+//else, if age is too big
+else {
+    console.log("You're an adult, nice !");
+}
